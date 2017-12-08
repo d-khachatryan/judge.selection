33bab51c8fe9e4a08bd11bc16cd9c69f30c8a7a7
davit.khachatryan <davit.khachatryan@hotmail.com>
2017-12-08 23:03:36 +0400

    Create Test Project

---
+++ stvsystem/stvsystem.UnitTests/SimpleUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace stvsystem.UnitTests
+{
+    [TestClass]
+    public class SimpleUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
