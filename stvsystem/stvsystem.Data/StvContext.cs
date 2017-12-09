d662a1c41afad04b4aa81cb785df06c57b3270df
davit.khachatryan <davit.khachatryan@hotmail.com>
2017-12-10 00:24:14 +0400

    STV-5 other changes

---
+++ stvsystem/stvsystem.Data/StvContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace stvsystem.Data
+{
+    public class StvContext : DbContext
+    {
+        public StvContext(DbContextOptions<StvContext> options)
+            : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CandidateConfiguration());
+        }
+    }
+}
