7a2182c7236e8f230a686f76ff689301460f2767
davit.khachatryan <davit.khachatryan@hotmail.com>
2017-12-10 00:20:47 +0400

    STV-5

---
+++ stvsystem/stvsystem.Database/dbo/Tables/Candidate.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Candidate]
+(
+	[CandidateID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [MiddleName] NVARCHAR(50) NULL
+)
--- stvsystem/stvsystem/Controllers/HomeController.cs
+++ stvsystem/stvsystem/Controllers/HomeController.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using stvsystem.Data;
 
 namespace stvsystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly StvContext _context;
+
+        public HomeController(StvContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            CandidateService service = new CandidateService(_context);
+            Candidate mock = new Candidate
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                MiddleName = "MiddleName"
+            };
+            service.InsertCandidate(mock);
             return View();
         }
 
