aae87e6b72e6a9f22e19f8ec43f48e2c077f4a38
davit.khachatryan <davit.khachatryan@hotmail.com>
2017-12-21 23:14:22 +0400

    STV-17

--- stvsystem/stvsystem.Data/Candidate.cs
+++ stvsystem/stvsystem.Data/Candidate.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace stvsystem.Data
 {
@@ -10,32 +11,28 @@ namespace stvsystem.Data
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int? GenderID { get; set; }
+        public int? CourtID { get; set; }
+        public int? SpecializationID { get; set; }
     }
 
-    public class CandidateConfiguration : IEntityTypeConfiguration<Candidate>
+    public class CandidateItem
     {
-        public void Configure(EntityTypeBuilder<Candidate> builder)
-        {
-            builder.ToTable("Candidate", schema: "dbo");
-            builder.HasKey(p => p.CandidateID);
-            builder.Property(p => p.CandidateID).ValueGeneratedOnAdd();
-            builder.Property(p => p.FirstName).HasMaxLength(50);
-            builder.Property(p => p.LastName).HasMaxLength(50);
-            builder.Property(p => p.MiddleName).HasMaxLength(50);
-        }
+        public int CandidateID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int? GenderID { get; set; }
+        public int? CourtID { get; set; }
+        public int? SpecializationID { get; set; }
+        public string CourtName { get; set; }
+        public string SpecializationName { get; set; }
+        public string GenderName { get; set; }
     }
 
-    public class CandidateService : ServiceBase
-    {
-        public CandidateService()
-            : base() { }
-        public CandidateService(StvContext context)
-            : base(context) { }
-        public Candidate InsertCandidate(Candidate item)
-        {
-            db.Candidates.Add(item);
-            db.SaveChanges();
-            return item;
-        }
-    }
+    
+
+    
 }
---
+++ stvsystem/stvsystem.Data/CandidateConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stvsystem.Data
+{
+    public class CandidateConfiguration : IEntityTypeConfiguration<Candidate>
+    {
+        public void Configure(EntityTypeBuilder<Candidate> builder)
+        {
+            builder.ToTable("Candidate", schema: "dbo");
+            builder.HasKey(p => p.CandidateID);
+            builder.Property(p => p.CandidateID).ValueGeneratedOnAdd();
+            builder.Property(p => p.FirstName).HasMaxLength(50);
+            builder.Property(p => p.LastName).HasMaxLength(50);
+            builder.Property(p => p.MiddleName).HasMaxLength(50);
+        }
+    }
+}
