8ca33832da8ed5dce7a053ef94f323ba60d3996a
davit.khachatryan <davit.khachatryan@hotmail.com>
2017-12-28 18:46:50 +0400

    Updated the search query

--- stvsystem/stvsystem.Data/JudgeService.cs
+++ stvsystem/stvsystem.Data/JudgeService.cs
@@ -14,77 +14,110 @@ namespace stvsystem.Data
         //    :base()
         //{
 
-        //}
-        public IList<JudgeItem> SearchJudgeSetItems()
+        public IList<JudgeItem> SearchJudges()
         {
-            IList<JudgeItem> result = new List<JudgeItem>();
-            result = db.Judges.Select(j => new JudgeItem
-            {
-                JudgeID = j.JudgeID,
-                FirstName = j.FirstName,
-                LastName = j.LastName,
-                MiddleName = j.MiddleName,
-                CourtID = j.CourtID,
-                GenderID = j.GenderID,
-                SpecializationID = j.SpecializationID
-            }).ToList();
+            IList<JudgeItem> result = (from judge in db.Judges
+                                       join t1 in db.Specializations on judge.SpecializationID equals t1.SpecializationID into r1
+                                       from specialization in r1.DefaultIfEmpty()
+                                       join t2 in db.Genders on judge.GenderID equals t2.GenderID into r2
+                                       from gender in r2.DefaultIfEmpty()
+                                       join t3 in db.Courts on judge.CourtID equals t3.CourtID into r3
+                                       from court in r3.DefaultIfEmpty()
+                                       select new
+                                       {
+                                           JudgeTable = judge,
+                                           GenderTable = gender,
+                                           CourtTable = court,
+                                           SpecializationTable = specialization
+                                       }).Select(list => new JudgeItem
+                                       {
+                                           JudgeID = list.JudgeTable.JudgeID,
+                                           FirstName = list.JudgeTable.FirstName,
+                                           LastName = list.JudgeTable.LastName,
+                                           MiddleName = list.JudgeTable.MiddleName,
+                                           CourtID = list.JudgeTable.CourtID,
+                                           CourtName = list.CourtTable.CourtName,
+                                           GenderID = list.JudgeTable.GenderID,
+                                           GenderName = list.GenderTable.GenderName,
+                                           SpecializationID = list.JudgeTable.SpecializationID,
+                                           SpecializationName = list.SpecializationTable.SpecializationName
+                                       }).ToList();
 
             return result;
-            //try
-            //{
-                //var judgeQuery =  in db.Judges select judge;
-                                 //    join t1 in db.Genders on judge.GenderID equals t1.GenderID into r1
-                                 //    from gender in r1.DefaultIfEmpty()
-                                 //    //join t2 in db.Regions on resident.RegionID equals t2.RegionID into r2
-                                 //    //from region in r2.DefaultIfEmpty()
-                                 //    //join t3 in db.Communities on resident.CommunityID equals t3.CommunityID into r3
-                                 //    //from community in r3.DefaultIfEmpty()
-                                 //select new
-                                 //{
-                                 //    JudgeTable = judge
-                                 //    //GenderTable = gender
-                                 //    //RegionTable = region,
-                                 //    //CommunityTable = community
-                                 //};
 
-
-                //if (judgeItem.FirstName != "")
-                //{
-                //    judgeQuery = from p in judgeQuery where p.FirstName.StartsWith(residentSearch.FirstName) select p;
-                //}
-                //if (judgeItem.LastName != "")
-                //{
-                //    judgeQuery = from p in judgeQuery where p.ResidentTable.LastName.StartsWith(residentSearch.LastName) select p;
-                //}
-
-                //IList<JudgeItem> result = judgeQuery.Select
-                //    (item => new ResidentSetItem
-                //    {
-                //        ResidentID = item.ResidentTable.ResidentID,
-                //        FirstName = item.ResidentTable.FirstName,
-                //        LastName = item.ResidentTable.LastName,
-                //        MiddleName = item.ResidentTable.MiddleName,
-                //        BirthDate = item.ResidentTable.BirthDate,
-                //        IdentificatorNumber = item.ResidentTable.IdentificatorNumber,
-                //        GenderID = item.ResidentTable.GenderID,
-                //        RegionID = item.ResidentTable.RegionID,
-                //        CommunityID = item.ResidentTable.CommunityID,
-                //        Street = item.ResidentTable.Street,
-                //        Building = item.ResidentTable.Building,
-                //        Home = item.ResidentTable.Home,
-                //        GenderName = item.GenderTable.GenderName,
-                //        RegionName = item.RegionTable.RegionName,
-                //        CommunityName = item.CommunityTable.CommunityName
-                //    }).ToList();
-
-            //    return result;
-            //}
-            //catch (Exception ex)
-            //{
-            //    exception = ex;
-            //    return null;
-            //}
         }
+
+        //}
+        //public IList<JudgeItem> SearchJudgeSetItems()
+        //{
+        //    IList<JudgeItem> result = new List<JudgeItem>();
+        //    result = db.Judges.Select(j => new JudgeItem
+        //    {
+        //        JudgeID = j.JudgeID,
+        //        FirstName = j.FirstName,
+        //        LastName = j.LastName,
+        //        MiddleName = j.MiddleName,
+        //        CourtID = j.CourtID,
+        //        GenderID = j.GenderID,
+        //        SpecializationID = j.SpecializationID
+        //    }).ToList();
+
+        //    return result;
+        //try
+        //{
+        //var judgeQuery =  in db.Judges select judge;
+        //    join t1 in db.Genders on judge.GenderID equals t1.GenderID into r1
+        //    from gender in r1.DefaultIfEmpty()
+        //    //join t2 in db.Regions on resident.RegionID equals t2.RegionID into r2
+        //    //from region in r2.DefaultIfEmpty()
+        //    //join t3 in db.Communities on resident.CommunityID equals t3.CommunityID into r3
+        //    //from community in r3.DefaultIfEmpty()
+        //select new
+        //{
+        //    JudgeTable = judge
+        //    //GenderTable = gender
+        //    //RegionTable = region,
+        //    //CommunityTable = community
+        //};
+
+
+        //if (judgeItem.FirstName != "")
+        //{
+        //    judgeQuery = from p in judgeQuery where p.FirstName.StartsWith(residentSearch.FirstName) select p;
+        //}
+        //if (judgeItem.LastName != "")
+        //{
+        //    judgeQuery = from p in judgeQuery where p.ResidentTable.LastName.StartsWith(residentSearch.LastName) select p;
+        //}
+
+        //IList<JudgeItem> result = judgeQuery.Select
+        //    (item => new ResidentSetItem
+        //    {
+        //        ResidentID = item.ResidentTable.ResidentID,
+        //        FirstName = item.ResidentTable.FirstName,
+        //        LastName = item.ResidentTable.LastName,
+        //        MiddleName = item.ResidentTable.MiddleName,
+        //        BirthDate = item.ResidentTable.BirthDate,
+        //        IdentificatorNumber = item.ResidentTable.IdentificatorNumber,
+        //        GenderID = item.ResidentTable.GenderID,
+        //        RegionID = item.ResidentTable.RegionID,
+        //        CommunityID = item.ResidentTable.CommunityID,
+        //        Street = item.ResidentTable.Street,
+        //        Building = item.ResidentTable.Building,
+        //        Home = item.ResidentTable.Home,
+        //        GenderName = item.GenderTable.GenderName,
+        //        RegionName = item.RegionTable.RegionName,
+        //        CommunityName = item.CommunityTable.CommunityName
+        //    }).ToList();
+
+        //    return result;
+        //}
+        //catch (Exception ex)
+        //{
+        //    exception = ex;
+        //    return null;
+        //}
+        //}
 
         public JudgeItem GetJudge(int? judgeID = null)
         {
