c1c17cc352a32018001df4b306bf74e6e741ffdc
davit.khachatryan <davit.khachatryan@hotmail.com>
2017-12-29 18:52:14 +0400

    bussines logic finished

--- stvsystem/stvsystem.Data/SettingService.cs
+++ stvsystem/stvsystem.Data/SettingService.cs
@@ -5,6 +5,34 @@ namespace stvsystem.Data
 {
     public class SettingService : ServiceBase
     {
+        public async Task<SettingItem> GetLatestSetting()
+        {
+            if (await Count() > 0)
+            {
+                Setting dbItem = await db.Settings.LastAsync();
+                SettingItem item = new SettingItem
+                {
+                    SettingID = dbItem.SettingID,
+                    SelectionName = dbItem.SelectionName,
+                    SelectionDate = dbItem.SelectionDate,
+                    StartTime = dbItem.StartTime,
+                    FinishTime = dbItem.FinishTime,
+                    SelectionCount = dbItem.SelectionCount,
+                    ParticipantCount = dbItem.ParticipantCount,
+                    SelectionStatus = dbItem.SelectionStatus
+                };
+                return item;
+            }
+            else
+            {
+                SettingItem item = new SettingItem
+                {
+                    SettingID = -1,
+                    SelectionStatus = SettingStatus.InPreparation
+                };
+                return item;
+            }            
+        }
         public SettingItem GetSetting(int settingID)
         {
             Setting dbItem = db.Settings.Find(settingID);
@@ -33,6 +61,7 @@ namespace stvsystem.Data
                 ParticipantCount = item.ParticipantCount,
                 SelectionStatus = SettingStatus.InPreparation
             };
+            db.Settings.RemoveRange(db.Settings);
             db.Settings.Add(dbItem);
             db.SaveChanges();
             item.SettingID = dbItem.SettingID;
@@ -79,6 +108,7 @@ namespace stvsystem.Data
             db.SaveChanges();
             return item;
         }
+                
         protected async Task<int> Count()
         {
             var count = await db.Settings.CountAsync();
