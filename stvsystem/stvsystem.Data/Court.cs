ef69afc8a0f239ce0569d990390e02f49f3e8ea4
Vahram Martinyan <vahram.martinyan@hotmail.com>
2017-12-29 13:08:16 +0400

    first

--- stvsystem/stvsystem.Data/Court.cs
+++ stvsystem/stvsystem.Data/Court.cs
@@ -6,4 +6,12 @@
         public int? CourtTypeID { get; set; }
         public string CourtName { get; set; }
     }
+
+    public class CourtItem
+    {
+        public int CourtID { get; set; }
+        public int? CourtTypeID { get; set; }
+        public string CourtTypeName { get; set; }
+        public string CourtName { get; set; }
+    }
 }
---
+++ stvsystem/stvsystem/Controllers/CourtController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using stvsystem.Data;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+
+namespace stvsystem.Controllers
+{
+    public class CourtController : Controller
+    {
+        CourtService service;
+        StvContext db;
+
+        public CourtController(StvContext context)
+        {
+            db = context;
+            service = new CourtService();
+        }
+
+        public JsonResult CourtList()
+        {
+            return Json(service.GetCourts());
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CourtSelect([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(service.GetCourts().ToDataSourceResult(request));
+        }
+
+        //[AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CourtCreate([DataSourceRequest] DataSourceRequest request, CourtItem courtItem)
+        {
+            if (courtItem != null && ModelState.IsValid)
+            {
+                service.CreateCourt(courtItem);
+            }
+            return Json(new[] { courtItem }.ToDataSourceResult(request, ModelState));
+        }
+
+        //[AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CourtUpdate([DataSourceRequest] DataSourceRequest request, CourtItem courtItem)
+        {
+            if (courtItem != null && ModelState.IsValid)
+            {
+                service.UpdateCourt(courtItem);
+            }
+            return Json(new[] { courtItem }.ToDataSourceResult(request, ModelState));
+        }
+
+        //[AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CourtDelete([DataSourceRequest] DataSourceRequest request, CourtItem courtItem)
+        {
+            if (courtItem != null)
+            {
+                service.DeleteCourt(courtItem);
+            }
+            return Json(new[] { courtItem }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
