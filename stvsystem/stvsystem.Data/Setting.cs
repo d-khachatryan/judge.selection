7e8a27dcaa4b6ac328d822ef8533ec2ff4b730a9
Vahram Martinyan <vahram.martinyan@hotmail.com>
2017-12-30 12:29:23 +0400

    STV-13-Vahram

--- stvsystem/stvsystem.Data/Setting.cs
+++ stvsystem/stvsystem.Data/Setting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace stvsystem.Data
@@ -8,16 +9,23 @@ namespace stvsystem.Data
     {
         public int SettingID { get; set; }
 
+        [Display(Name = "Ընտրության անվանում")]
         public string SelectionName { get; set; }
 
+        [Display(Name = "Ընտրության ա/թ")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? SelectionDate { get; set; }
 
+        [Display(Name = "Մեկնարկի ժամը")]
         public TimeSpan? StartTime { get; set; }
 
+        [Display(Name = "Ավարտի ժամը")]
         public TimeSpan? FinishTime { get; set; }
 
+        [Display(Name = "Թեկնածուների թիվը")]
         public int? SelectionCount { get; set; }
 
+        [Display(Name = "Մասնակիցների թիվը")]
         public int? ParticipantCount { get; set; }
 
         public SettingStatus SelectionStatus { get; set; }
@@ -27,16 +35,29 @@ namespace stvsystem.Data
     {
         public int SettingID { get; set; }
 
+        [Display(Name = "Ընտրության անվանում")]
+        [Required(ErrorMessage = "Ընտրության անվանումը պարտադիր է")]
         public string SelectionName { get; set; }
 
+        [Display(Name = "Ընտրության ա/թ")]
+        [Required(ErrorMessage = "Ընտրության ամսաթիվը պարտադիր է")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? SelectionDate { get; set; }
 
+        [Display(Name = "Մեկնարկի ժամը")]
+        [Required(ErrorMessage = "Մեկնարկի ժամը պարտադիր է")]
         public TimeSpan? StartTime { get; set; }
 
+        [Display(Name = "Ավարտի ժամը")]
+        [Required(ErrorMessage = "Ավարտի ժամը պարտադիր է")]
         public TimeSpan? FinishTime { get; set; }
 
+        [Display(Name = "Թեկնածուների թիվը")]
+        [Required(ErrorMessage = "Թեկնածուների թիվը պարտադիր է")]
         public int? SelectionCount { get; set; }
 
+        [Display(Name = "Մասնակիցների թիվը")]
+        [Required(ErrorMessage = "Մասնակիցների թիվը պարտադիր է")]
         public int? ParticipantCount { get; set; }
 
         public SettingStatus SelectionStatus { get; set; }
