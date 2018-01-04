044bb18c9580738cb2c97e1dba181f065d913f10
Vahram Martinyan <vahram.martinyan@hotmail.com>
2018-01-04 23:35:50 +0400

    STV-24 included PDF form

--- stvsystem/stvsystem/Controllers/CredentialController.cs
+++ stvsystem/stvsystem/Controllers/CredentialController.cs
@@ -3,19 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using stvsystem.Data;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
 
 namespace stvsystem.Controllers
 {
     public class CredentialController : Controller
     {
-        public IActionResult Index()
+        CredentialService service;
+        SettingService settingService;
+        StvContext db;
+
+        public CredentialController(StvContext context)
         {
-            return View();
+            db = context;
+            service = new CredentialService();
+            settingService = new SettingService();
         }
 
-        public IActionResult Detail()
+        public IActionResult Index()
         {
-            return View();
+            if(db.Settings.Count() != 0) {
+                var setting = db.Settings.First();
+                return View(setting);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult FilterCredential([DataSourceRequest]DataSourceRequest request, string password, string idStr)
+        {
+            DataSourceResult result = service.Search(password, idStr).ToDataSourceResult(request);
+            return Json(result);
+        }
+
+        public IActionResult Detail(string start, string end)
+        {
+            IList<CredentialItem> credentials = service.Details(start, end);
+            Task<SettingItem> setting = settingService.GetLatestSetting();
+            ViewBag.Setting = setting.Result;
+            //ViewBag.Setting = db.Settings.Last();
+            return View(credentials);
+        }
+
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
         }
 
         [HttpPost ]
@@ -23,7 +62,5 @@ namespace stvsystem.Controllers
         {
             return View();
         }
-
-
     }
 }
