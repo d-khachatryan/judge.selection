5ee49f75e6dda4c7a9e507bd3528d905ae6dcabd
Arman Ohanyan <arman.ohanyan@hotmail.com>
2018-01-24 14:31:45 +0400

    Done

---
+++ stvsystem/stvsystem.Data/SummaryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace stvsystem.Data
+{
+    public class SummaryConfiguration : IEntityTypeConfiguration<Summary>
+    {
+        public void Configure(EntityTypeBuilder<Summary> builder)
+        {
+            builder.ToTable("vwResult", schema: "dbo");
+            builder.HasKey(p => p.CandidateID);
+        }
+    }
+}
--- stvsystem/stvsystem.Database/dbo/vwResult.sql
+++ stvsystem/stvsystem.Database/dbo/vwResult.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vwResult]
 	AS SELECT CandidateID, 
-[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15]
+[1] AS N1, [2] AS N2, [3] AS N3, [4] AS N4, [5] AS N5, [6] AS N6, [7] AS N7, [8]AS N8, [9] AS N9, [10] AS N10, [11] AS N11, [12] AS N12, [13] AS N13, [14] AS N14, [15] AS N15
 FROM
 (SELECT CredentialID, CandidateID, CandidateIndex 
     FROM [dbo].[Result]) AS SourceTable
